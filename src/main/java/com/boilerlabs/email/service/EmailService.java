6b87418275d06ac492bd4813e9d7f3fcabc085fc
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-10-02 21:41:08 -0300

    feat: update EmailService
    Added sendTemplateMail

--- src/main/java/com/boilerlabs/email/service/EmailService.java
+++ src/main/java/com/boilerlabs/email/service/EmailService.java
@@ -3,10 +3,13 @@ package com.boilerlabs.email.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.boilerlabs.email.record.EmailRecord;
+
+import jakarta.mail.internet.MimeMessage;
 
 @Service
 @Async
@@ -16,6 +19,22 @@ public class EmailService {
     private JavaMailSender javaMailSender;
 
     private final String fromNoreply = "noreply@boilerlabs.com";
+
+    public void sendTemplateMail(EmailRecord emailRecord, String htmlContent) {
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            helper.setFrom(fromNoreply);
+            helper.setTo(emailRecord.to());
+            helper.setSubject(emailRecord.subject());
+            helper.setText(htmlContent, true);
+
+            javaMailSender.send(message);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to send email", e);
+        }
+    }
 
     public void sendSimpleEmail(EmailRecord emailRecord) {
         try {
